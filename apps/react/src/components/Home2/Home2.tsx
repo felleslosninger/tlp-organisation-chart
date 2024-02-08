ff282bf11de0c10c415f18c3fa8c992b7b160f66
Jens Mo <jens19mo98@gmail.com>
2024-02-08 12:15:52 +0100

    More test data

--- apps/react/src/components/Home2/Home2.tsx
+++ apps/react/src/components/Home2/Home2.tsx
@@ -1,15 +1,34 @@
-import React, { useEffect } from "react";
-import jsonData from "../../data/data.json";
+import React, { useEffect, useState } from "react";
+import jsonData1 from "../../data/data1.json";
+import jsonData2 from "../../data/data2.json";
+import jsonData3 from "../../data/data3.json";
 import { generateOrgChart } from "../../generateOrgChart";
+import { Button } from "@digdir/design-system-react";
 
 const Home = () => {
+  const [data, setData] = useState(jsonData1);
+
   useEffect(() => {
-    generateOrgChart(jsonData, "test");
+    generateOrgChart(data, "test");
   });
 
   return (
     <div>
       <h1>Lag organisasjonsKart</h1>
+      <div
+        style={{
+          margin: "50px 0 50px 0",
+          display: "flex",
+          gap: "50px",
+          width: "100%",
+          alignItems: "center",
+          borderBottom: "1px solid #000",
+        }}
+      >
+        <Button onClick={() => setData(jsonData1)}>Data 1</Button>
+        <Button onClick={() => setData(jsonData2)}>Data 2</Button>
+        <Button onClick={() => setData(jsonData3)}>Data 3</Button>
+      </div>
       <div id="test"></div>
     </div>
   );
