ec2fc8b9f0d89c595c21081bc6f3d5419700992a
Jens Mo <jens19mo98@gmail.com>
2024-02-23 13:26:00 +0100

    Created data fil to display different variations with specialColumn

--- apps/react/src/components/Home/Home.tsx
+++ apps/react/src/components/Home/Home.tsx
@@ -5,6 +5,7 @@ import jsonData2 from "../../data/data2.json";
 import jsonData4 from "../../data/data4.json";
 import jsonData5 from "../../data/data5.json";
 import jsonData6 from "../../data/data6.json";
+import jsonData7 from "../../data/data7.json";
 import jsonDigdir1 from "../../data/digdir1.json";
 import jsonDigdir2 from "../../data/digdir2.json";
 import jsonDigdir3 from "../../data/digdir3.json";
@@ -12,7 +13,7 @@ import { generateOrgChart } from "../../function/generateOrgChartV2";
 import { Button } from "@digdir/design-system-react";
 
 const Home = () => {
-  const [data, setData] = useState(jsonDigdir3);
+  const [data, setData] = useState(jsonData7);
 
   useEffect(() => {
     generateOrgChart(data, "test");
@@ -31,15 +32,16 @@ const Home = () => {
           borderBottom: "1px solid #000",
         }}
       >
+        <Button onClick={() => setData(jsonDigdir1)}>Digdir 1</Button>
+        <Button onClick={() => setData(jsonDigdir2)}>Digdir 2</Button>
+        <Button onClick={() => setData(jsonDigdir3)}>Digdir 3</Button>
         <Button onClick={() => setData(jsonData1)}>Data 1</Button> 
         <Button onClick={() => setData(jsonData2)}>Data 2</Button>
         {/* <Button onClick={() => setData(jsonData3)}>Data 3</Button> */}
         <Button onClick={() => setData(jsonData4)}>Data 4</Button>  
         <Button onClick={() => setData(jsonData5)}>Data 5</Button>
         <Button onClick={() => setData(jsonData6)}>Data 6</Button>
-        <Button onClick={() => setData(jsonDigdir1)}>Digdir 1</Button>
-        <Button onClick={() => setData(jsonDigdir2)}>Digdir 2</Button>
-        <Button onClick={() => setData(jsonDigdir3)}>Digdir 3</Button>
+        <Button onClick={() => setData(jsonData7)}>Data 7</Button>
       </div>
       <div id="test"></div>
     </div>
