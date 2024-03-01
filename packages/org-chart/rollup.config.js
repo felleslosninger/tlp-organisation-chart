f3392bfde0d3b42943972a8ef3a123bb749341fb
Øyvind Thune <oyvlei@gmail.com>
2024-03-01 09:33:19 +0100

    Changed outfile JS file to index.js

--- packages/org-chart/rollup.config.js
+++ packages/org-chart/rollup.config.js
@@ -5,7 +5,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 export default {
   input: "./tsc-build/index.js",
   output: {
-    file: "dist/bundle.js",
+    file: "dist/index.js",
     format: "es",
   },
   plugins: [peerDepsExternal(), resolve(), commonjs()],
