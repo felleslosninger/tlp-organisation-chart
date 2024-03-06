bcc6e7d1d10f47826ee3357af7e5afe87a7a320f
Barsnes <tobias.barsnes@digdir.no>
2024-03-06 15:20:58 +0100

    cleanup rollup, remove unused packages

--- packages/org-chart/rollup.config.js
+++ packages/org-chart/rollup.config.js
@@ -1,29 +1,14 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
-import copy from 'rollup-plugin-copy'
+import copy from 'rollup-plugin-copy';
 
 export default {
-  input: "./tsc-build/src/index.js",
+  input: './tsc-build/index.js',
   output: {
-    file: "dist/index.js",
-    format: "es",
+    file: 'dist/index.js',
+    format: 'es',
   },
   plugins: [
-    postcss({
-      extract: "dist/style.css",
-      minimize: true,
-      inject: false,
-      path: "./src/style.css",
-    }),
     copy({
-      targets: [
-        { src: 'src/style.css', dest: 'dist' }
-      ]
+      targets: [{ src: 'src/style.css', dest: 'dist' }],
     }),
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
   ],
 };
