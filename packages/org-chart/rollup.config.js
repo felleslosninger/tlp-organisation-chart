b31d93de537dcdbb005346c6b6a0de8b828f1b85
Barsnes <tobias.barsnes@digdir.no>
2024-03-06 15:29:03 +0100

    add terser, add sroucemaps

--- packages/org-chart/rollup.config.js
+++ packages/org-chart/rollup.config.js
@@ -1,14 +1,21 @@
 import copy from 'rollup-plugin-copy';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: './tsc-build/index.js',
   output: {
     file: 'dist/index.js',
     format: 'es',
+    plugins: [terser()],
   },
   plugins: [
     copy({
-      targets: [{ src: 'src/style.css', dest: 'dist' }],
+      targets: [
+        {
+          src: 'src/style.css',
+          dest: 'dist',
+        },
+      ],
     }),
   ],
 };
