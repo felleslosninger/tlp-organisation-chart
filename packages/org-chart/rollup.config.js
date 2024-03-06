6299acf49f6b5a4fc217e38e15a1653e17cdadc7
Barsnes <tobias.barsnes@digdir.no>
2024-03-06 12:47:00 +0100

    chore: Update watch script

--- packages/org-chart/rollup.config.js
+++ packages/org-chart/rollup.config.js
@@ -2,18 +2,25 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
-import path from "path";
+import copy from 'rollup-plugin-copy'
 
 export default {
-  input: "./tsc-build/index.js",
+  input: "./tsc-build/src/index.js",
   output: {
     file: "dist/index.js",
     format: "es",
   },
   plugins: [
     postcss({
-      extract: path.resolve("dist/style.css"),
+      extract: "dist/style.css",
       minimize: true,
+      inject: false,
+      path: "./src/style.css",
+    }),
+    copy({
+      targets: [
+        { src: 'src/style.css', dest: 'dist' }
+      ]
     }),
     peerDepsExternal(),
     resolve(),
