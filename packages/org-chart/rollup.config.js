b336f41dbefd9d38d104e7da55d549cc89082161
Tobias Barsnes <tobiasbarsnes@gmail.com>
2024-03-07 08:45:30 +0100

    build: Correct build-flow and settings (#26)

--- packages/org-chart/rollup.config.js
+++ packages/org-chart/rollup.config.js
@@ -1,29 +1,23 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
-import copy from 'rollup-plugin-copy'
+import copy from 'rollup-plugin-copy';
+import terser from '@rollup/plugin-terser';
 
-export default {
-  input: "./tsc-build/src/index.js",
-  output: {
-    file: "dist/index.js",
-    format: "es",
+export default [
+  {
+    input: './tsc-build/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'es',
+      plugins: [terser()],
+    },
+    plugins: [
+      copy({
+        targets: [
+          {
+            src: 'src/index.css',
+            dest: 'dist',
+          },
+        ],
+      }),
+    ],
   },
-  plugins: [
-    postcss({
-      extract: "dist/style.css",
-      minimize: true,
-      inject: false,
-      path: "./src/style.css",
-    }),
-    copy({
-      targets: [
-        { src: 'src/style.css', dest: 'dist' }
-      ]
-    }),
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-  ],
-};
+];
