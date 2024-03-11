241f105b2aae67a495abfcba49785992d848dc61
Barsnes <tobias.barsnes@digdir.no>
2024-03-11 12:37:41 +0100

    add mdx support

--- apps/storefront/vite.config.ts
+++ apps/storefront/vite.config.ts
@@ -1,9 +1,15 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import mdx from '@mdx-js/rollup';
+import { vitePlugin as remix } from '@remix-run/dev';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    mdx({
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+    }),
     remix({
       ssr: false,
     }),
