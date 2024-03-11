d08150fc7c7140ef67525f578d5e2830f0a227ca
Barsnes <tobias.barsnes@digdir.no>
2024-03-11 16:17:10 +0100

    shields

--- apps/storefront/app/routes/_index.tsx
+++ apps/storefront/app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from '@remix-run/node';
 
 import { Chart } from '../components/Chart.client';
+import { Heading, Link } from '@digdir/designsystemet-react';
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,5 +14,25 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return <Chart />;
+  return (
+    <div>
+      <Heading
+        level={1}
+        spacing
+      >
+        Organisation Chart{' '}
+        <Link
+          href='https://www.npmjs.com/package/@digdir/organisation-chart'
+          target='_blank'
+          title='Latest release of npm package'
+        >
+          <img
+            src='https://img.shields.io/npm/v/@digdir/organisation-chart?label=latest%20release&color=0051be'
+            alt='Latest release of npm package'
+          />
+        </Link>
+      </Heading>
+      <Chart />
+    </div>
+  );
 }
