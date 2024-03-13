761fabe90a478c1742dac5ddcca3e351cbde3384
Barsnes <tobias.barsnes@digdir.no>
2024-03-13 11:31:01 +0100

    remove frontmatter

--- apps/storefront/vite.config.ts
+++ apps/storefront/vite.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 import mdx from '@mdx-js/rollup';
 import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
 import rehypeAutoLinkHeadings from 'rehype-autolink-headings';
@@ -12,7 +11,7 @@ import rehypeAutoLinkHeadings from 'rehype-autolink-headings';
 export default defineConfig({
   plugins: [
     mdx({
-      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter, remarkGfm],
+      remarkPlugins: [remarkFrontmatter, remarkGfm],
       rehypePlugins: [rehypeHighlight, rehypeAutoLinkHeadings],
       providerImportSource: '@mdx-js/react',
     }),
