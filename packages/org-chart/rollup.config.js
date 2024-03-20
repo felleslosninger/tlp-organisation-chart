2d6db341f0e20dc4e594a41f4cc7c8588e703633
Barsnes <tobias.barsnes@digdir.no>
2024-03-20 12:53:58 +0100

    build: Build files for es and cjs

--- packages/org-chart/rollup.config.js
+++ packages/org-chart/rollup.config.js
@@ -4,11 +4,18 @@ import terser from '@rollup/plugin-terser';
 export default [
   {
     input: './tsc-build/index.js',
-    output: {
-      file: 'dist/index.js',
-      format: 'es',
-      plugins: [terser()],
-    },
+    output: [
+      {
+        file: 'dist/es/index.js',
+        format: 'es',
+        plugins: [terser()],
+      },
+      {
+        file: 'dist/cjs/index.js',
+        format: 'cjs',
+        plugins: [terser()],
+      },
+    ],
     plugins: [
       copy({
         targets: [
