1073d19c2ae36753e57a8cff8165fe41b9ca2dd3
Jens Mo <jens19mo98@gmail.com>
2024-03-21 10:10:33 +0100

    prefix included with aria-owns

--- packages/org-chart/src/orgChart.ts
+++ packages/org-chart/src/orgChart.ts
@@ -131,11 +131,14 @@ export function generateOrgChart(data: OrgChartData, containerId: string) {
       innerNode.id = `${idPrefix}-${nodeData.id}`;
 
       if (node.component?.children && !isRoot) {
-        innerNode.setAttribute('aria-owns', node.component.children.join(' '));
+        innerNode.setAttribute(
+          'aria-owns',
+          createChildrenList(node.component.children, idPrefix).join(' '),
+        );
       } else if (isRoot) {
         innerNode.setAttribute(
           'aria-owns',
-          getRootChildren(currentLayout).join(' '),
+          getRootChildren(currentLayout, idPrefix).join(' '),
         );
       }
 
@@ -231,7 +234,7 @@ export function generateOrgChart(data: OrgChartData, containerId: string) {
       if (column.component?.children) {
         nodesWrapper.setAttribute(
           'aria-owns',
-          column.component.children.join(' '),
+          createChildrenList(column.component.children, idPrefix).join(' '),
         );
       }
 
@@ -2389,18 +2392,26 @@ function getChildArrowNavigation(
   return dataAttributes;
 }
 
-function getRootChildren(currentLayout: Layout): string[] {
+function getRootChildren(currentLayout: Layout, idPrefix: string): string[] {
   const rootChildren: string[] = [];
 
   // Iterate through the rows and columns in the layout
   for (const row of currentLayout.rows) {
     for (const column of row.row) {
       // Add all the ids in the column to the rootChildren array
-      rootChildren.push(...column.id);
+      rootChildren.push(...column.id.map((id) => `${idPrefix}-${id}`));
     }
   }
   // Remove the first id in the array, which is the root id
   rootChildren.shift();
 
   return rootChildren;
+}
+
+function createChildrenList(children: string[], idPrefix: string) {
+  const childrenList: string[] = [];
+  children.forEach((childId: string) => {
+    childrenList.push(`${idPrefix}-${childId}`);
+  });
+  return childrenList;
 }
