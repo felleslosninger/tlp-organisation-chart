d64166d6ad2ecf745ce721bafe7144c6f11d292a
Jens Mo <jens19mo98@gmail.com>
2024-03-21 09:18:36 +0100

    fixed arrow navogation when row only contains one colum, corrected more test data

--- packages/org-chart/src/orgChart.ts
+++ packages/org-chart/src/orgChart.ts
@@ -2303,6 +2303,7 @@ function getArrowNavigaitonData(
           layout.rows[currentRowIndex - 1].row[previousRowLength - 1].id
             .length - 1
         ];
+      //const nextRowFirstItem = layout.rows[currentRowIndex + 1].row[0].id[0];
 
       if (siblingsAmount > 1) {
         // If the node is the first in the row and there are more than one sibling, add data attributes for arrow right and left navigation
@@ -2310,6 +2311,20 @@ function getArrowNavigaitonData(
           dataAttributes,
           'data-arrow-right',
           layout.rows[currentRowIndex].row[indexInRow].id[0],
+        );
+        addDataAttribute(
+          dataAttributes,
+          'data-arrow-left',
+          previousRowLastItem,
+        );
+      } else if (siblingsAmount === 1 && !isRoot) {
+        console.log(layout.rows[currentRowIndex]);
+        console.log('test');
+
+        addDataAttribute(
+          dataAttributes,
+          'data-arrow-right',
+          !isLastRow ? layout.rows[currentRowIndex + 1].row[0].id[0] : '',
         );
         addDataAttribute(
           dataAttributes,
