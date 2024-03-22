1cdaacfc2df8a0bb1a89a67d0383f0c1663e29a0
Jens Mo <jens19mo98@gmail.com>
2024-03-22 12:37:56 +0100

    Implemented select from ds, to switch between charts

--- apps/storefront/app/components/Chart/Chart.client.tsx
+++ apps/storefront/app/components/Chart/Chart.client.tsx
@@ -1,26 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { NativeSelect } from '@digdir/designsystemet-react';
 
 import { generateOrgChart } from '@digdir/organisation-chart';
 import '@digdir/organisation-chart/dist/index.css';
 
-import data from '../../data/avdelinger-og-seksjoner.json';
+import avdelinger from '../../data/avdelinger-og-seksjoner.json';
+import leveranseomrader from '../../data/leveranseomrader.json';
 
 import classes from './Chart.module.css';
 
 export const Chart = () => {
+  const [data, setData] = useState(avdelinger);
   useEffect(() => {
     generateOrgChart(data, 'chart');
-  }, []);
+  }, [data]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    if (value === '1') {
+      setData(avdelinger);
+    } else if (value === '2') {
+      setData(leveranseomrader);
+    }
+  };
 
   return (
     <>
-      <NativeSelect label='Velg ett kart'>
-        <option value='1'>Avdelinger og seksjoner</option>
-        <option value='2'>Leveranseområder</option>
-      </NativeSelect>
-      <p></p>
+      <div className={classes.select}>
+        <NativeSelect
+          label='Velg ett kart'
+          onChange={handleChange}
+        >
+          <option value='1'>Avdelinger og seksjoner</option>
+          <option value='2'>Leveranseområder</option>
+        </NativeSelect>
+      </div>
       <div className={classes.wrapper}>
         <div id='chart' />
       </div>
