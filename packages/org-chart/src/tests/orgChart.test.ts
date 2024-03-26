b4c0d3cba13ce34844fdf2e0a1e5918c45dadb43
Barsnes <tobias.barsnes@digdir.no>
2024-03-26 13:40:09 +0100

    chore: Update test

--- packages/org-chart/src/tests/orgChart.test.ts
+++ packages/org-chart/src/tests/orgChart.test.ts
@@ -5,17 +5,23 @@ import ResizeObserver from 'resize-observer-polyfill';
 global.ResizeObserver = ResizeObserver;
 
 describe('generateOrgChart', () => {
+  
+  beforeAll(() => {
+    return new Promise<void>((resolve) => {
+      const container = document.createElement('div');
+      container.id = 'test-container';
+      document.body.appendChild(container);
+      const containerId = 'test-container';
+
+      generateOrgChart(data, containerId);
+
+      resolve();
+    });
+  });
+
   it('should generate an org chart correctly', () => {
-
-    const container = document.createElement('div');
-    container.id = 'test-container';
-    document.body.appendChild(container);
-    const containerId = 'test-container';
-
-    generateOrgChart(data, containerId);
-
-    expect(document.getElementById('root')).toBeDefined();
-    expect(document.getElementById('item1')).toBeDefined();
+    // check if the root element to have aria-level 1
+    expect(document.getElementById('och-root')?.getAttribute('aria-level')).toBe('1');
 
     // expect role tree and treeitem to be in the DOM
     expect(document.querySelector('[role="tree"]')).toBeDefined();
