4255561a19eb36d57c3dd83d66a92fc7e6f2e2cd
Jens Mo <jens19mo98@gmail.com>
2024-04-02 10:35:39 +0200

    arrow navigation testing

--- packages/org-chart/src/tests/orgChart.test.ts
+++ packages/org-chart/src/tests/orgChart.test.ts
@@ -51,4 +51,61 @@ describe('generateOrgChart', () => {
     const node = document.getElementById('och-root');
     expect(node).toBeDefined();
   });
+
+  //och-item2 should have data property data-arrow-down="och-child3, data-arrow-left="och-item1", data-arrow-right="och-item3"
+  it('should have the correct data properties', () => {
+    const node = document.getElementById('och-item2');
+    expect(node).toBeDefined();
+    expect(node?.getAttribute('data-arrow-down')).toBe('och-child3');
+    expect(node?.getAttribute('data-arrow-left')).toBe('och-item1');
+    expect(node?.getAttribute('data-arrow-right')).toBe('och-item3');
+  });
+
+  //testing special column data properties
+  //och-item4 should have data property data-arrow-down="och-child1, data-arrow-left="och-item3", data-arrow-right="och-item5"
+  it('should have the correct data properties for special columns', () => {
+    const node = document.getElementById('och-item4');
+    expect(node).toBeDefined();
+    expect(node?.getAttribute('data-arrow-down')).toBe('och-child1');
+    expect(node?.getAttribute('data-arrow-left')).toBe('och-item3');
+    expect(node?.getAttribute('data-arrow-right')).toBe('och-item5');
+  });
+
+  //special column child should have data property data-arrow-up="och-item5"
+  it('should have the correct data properties for special column children', () => {
+    const node = document.getElementById('och-child1');
+    expect(node).toBeDefined();
+    expect(node?.getAttribute('data-arrow-up')).toBe('och-item5');
+  });
+
+  //testing if the focus moves correctly
+  it('should move the focus to the first node when home is pressed', () => {
+    const root = document.getElementById('och-item1');
+    root?.focus();
+    root?.dispatchEvent(new KeyboardEvent('keyup', { key: 'Home' }));
+    setTimeout(() => {
+      const firstNode = document.getElementById('och-item1');
+      expect(document.activeElement).toBe(firstNode);
+    }, 0);
+  });
+
+  it('should move the focus to the last node when end is pressed', () => {
+    const root = document.getElementById('och-root');
+    root?.focus();
+    root?.dispatchEvent(new KeyboardEvent('keyup', { key: 'End' }));
+    setTimeout(() => {
+      const lastNode = document.getElementById('och-item6');
+      expect(document.activeElement).toBe(lastNode);
+    }, 0);
+  });
+
+  //if the focus is on the last node, and arrow right is pressed, the focus should stay on the last node
+  it('should keep the focus on the last node when arrow right is pressed', () => {
+    const lastNode = document.getElementById('och-item6');
+    lastNode?.focus();
+    lastNode?.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowRight' }));
+    setTimeout(() => {
+      expect(document.activeElement).toBe(lastNode);
+    }, 0);
+  });
 });
